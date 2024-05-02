d50b8db74f046d387aa9ac99dd18aecd82ddb062
nom utilisateur de votre repo git <morzak78@gmail.com>
2024-05-02 18:47:41 +0200

    🚧 F02-Base-Layout: Add DesktopNavbar structure

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -5,7 +5,7 @@ export default function Logo() {
   return (
     <a href="/" className='flex items-center gap-[4px]'>
         <WalletMinimal className='stroke h-11 w-11 stroke-green-500 stroke-[1.5]' />
-        <p className='bg-gradient-to-tr from-green-500 to-green-500 bg-clip-text text-3xl font-bold tracking-tighter text-transparent'>CashTrack</p>
+        <p className='bg-gradient-to-tr from-green-600 to-green-300 bg-clip-text text-3xl font-bold tracking-tighter text-transparent'>CashTrack</p>
     </a>
   )
 }
