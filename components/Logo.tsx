db8dee65ab13fcb1a48053d112f5fca93fc4e973
nom utilisateur de votre repo git <morzak78@gmail.com>
2024-05-02 15:40:23 +0200

    💄  F01-Authentification: Add new Logo color

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -3,9 +3,9 @@ import React from 'react'
 
 export default function Logo() {
   return (
-    <a href="/" className='flex items-center gap-2'>
-        <WalletMinimal className='stroke h-11 w-11 stroke-amber-500 stroke-[1.5]' />
-        <p className='bg-gradient-to-r from-amber-400 to-orange-500 bg-clip-text text-3xl font-bold leading-tight tracking-tighter text-transparent'>CashTrack</p>
+    <a href="/" className='flex items-center gap-[4px]'>
+        <WalletMinimal className='stroke h-11 w-11 stroke-green-500 stroke-[1.5]' />
+        <p className='bg-gradient-to-tr from-green-500 to-green-500 bg-clip-text text-3xl font-bold tracking-tighter text-transparent'>CashTrack</p>
     </a>
   )
 }
