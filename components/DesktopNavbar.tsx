7c88519b8e0adacecf1676ae18eae198bc88ff07
nom utilisateur de votre repo git <morzak78@gmail.com>
2024-05-02 18:05:08 +0200

    🚧 F02-Base-Layout: Add dashboard folder and link it with Navbar

---
+++ components/DesktopNavbar.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function DesktopNavbar() {
+  return (
+    <div className='hidden border-separate border-b bg-background md:block'></div>
+  )
+}
---
+++ components/Navbar.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import DesktopNavbar from './DesktopNavbar'
+
+export default function Navbar() {
+  return (
+    <>
+        <DesktopNavbar />
+    </>
+  )
+}
