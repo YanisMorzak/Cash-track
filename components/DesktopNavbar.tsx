838933bb9faada2cf041447e60dfdc20e9307621
nom utilisateur de votre repo git <morzak78@gmail.com>
2024-05-02 18:50:08 +0200

    🚧 F02-Base-Layout: map items data in DesktopNavbar

--- components/DesktopNavbar.tsx
+++ components/DesktopNavbar.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import Logo from './Logo'
+import { items } from '@/enums/itemsNav'
+import { Label } from '@/components/ui/label';
 
 export default function DesktopNavbar() {
   return (
@@ -7,7 +9,11 @@ export default function DesktopNavbar() {
         <nav className="container flex items-center justify-between px-8">
             <div className="flex h-[80px] min-h-[60px] items-center gap-x-4">
                 <Logo />
-                <div className="flex h-full"></div>
+                <div className="flex h-full">
+                    {items.map((item) => {
+                        return <NavbarItem key={item.label} link={item.link} Label={item.label}/>
+                    })}
+                </div>
             </div>
         </nav>
     </div>
