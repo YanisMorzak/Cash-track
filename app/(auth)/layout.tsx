9614917140ec2def0c211d65e482a797cdf51061
nom utilisateur de votre repo git <morzak78@gmail.com>
2024-05-02 15:03:16 +0200

    🚧 F01-Authentification: create layout to (auth) folder

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,11 @@
+import React, { ReactNode } from 'react'
+
+export default function layout({children}: {children: ReactNode}) {
+  return (
+    <div className='relative flex h-screen w-full flex-col items-center justify-center'>
+        <div className="mt-12">
+            {children}
+        </div>
+    </div>
+  )
+}
