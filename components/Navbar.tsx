8d9f2be6fce22d79b08d268cfc377d55e516955f
nom utilisateur de votre repo git <morzak78@gmail.com>
2024-05-02 20:14:32 +0200

    🚧   F02-Base-Layout: add MobileNavbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,11 +2,13 @@
 
 import React from 'react'
 import DesktopNavbar from './DesktopNavbar'
+import MobileNavbar from './MobileNavbar'
 
 export default function Navbar() {
   return (
     <>
         <DesktopNavbar />
+        <MobileNavbar />
     </>
   )
 }
