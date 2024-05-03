9a585f4ec0ae13db910cda27261e7627429593d5
nom utilisateur de votre repo git <morzak78@gmail.com>
2024-05-03 15:19:26 +0200

    🚧     F04-Wizard-page: use curentUser() method and add a condition which check if the user is defined or not

--- app/wizard/page.tsx
+++ app/wizard/page.tsx
@@ -1,6 +1,14 @@
+import { currentUser } from '@clerk/nextjs/server'
+import { redirect } from 'next/navigation'
 import React from 'react'
 
-export default function page() {
+export default async function page() {
+    const user = await currentUser()
+
+    // if the user is not defined we are going to redirect him to sign in page
+    if (!user) {
+        redirect("/sign-in")
+    }
   return (
     <div>page</div>
   )
