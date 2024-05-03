4ee9360470f3333c0af00dc111156d75790ebfcf
nom utilisateur de votre repo git <morzak78@gmail.com>
2024-05-03 15:28:35 +0200

    🚧     F04-Wizard-page: Add content top page and use many of ui components

--- app/wizard/page.tsx
+++ app/wizard/page.tsx
@@ -1,3 +1,5 @@
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
+import { Separator } from '@/components/ui/separator'
 import { currentUser } from '@clerk/nextjs/server'
 import { redirect } from 'next/navigation'
 import React from 'react'
@@ -10,6 +12,30 @@ export default async function page() {
         redirect("/sign-in")
     }
   return (
-    <div>page</div>
+    <div className='container flex max-w-2xl flex-col items-center justify-between gap-4'>
+        <div>
+            <h1 className='text-center text-3xl'>
+            Welcome, <span className="ml-2 font-bold">{user.firstName}!</span>
+            </h1>
+            <h2 className="mt-4 text-center text-base text-muted-foreground">
+              Let &apos;s get started by setting up your currency
+            </h2>
+            <h3 className="mt-2 text-center text-sm text-muted-foreground">
+              You can change these settings at any time
+            </h3>
+        </div>
+        <Separator />
+        <Card className='w-full'>
+        <CardHeader>
+          <CardTitle>Currency</CardTitle>
+          <CardDescription>
+            Set your default currency for transactions
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          {/* <CurrencyComboBox /> */}
+        </CardContent>
+        </Card>
+    </div>
   )
 }
