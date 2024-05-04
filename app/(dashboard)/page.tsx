8eb4d192e5c14ef3730a72e30049a8fdcbe26a58
nom utilisateur de votre repo git <morzak78@gmail.com>
2024-05-04 13:53:02 +0200

    🚧     F05-Dashboard-intro:  retrieves the currently logged-in user using the "currentUser()" function

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,6 +1,12 @@
+import prisma from '@/lib/prisma';
+import { currentUser } from '@clerk/nextjs/server';
+import { redirect } from 'next/navigation';
 import React from 'react'
 
-export default function page() {
+export default async function page() {
+  //retrieves the currently logged-in user using the "currentUser()" function
+  const user = await currentUser();
+
   return (
     <div>page</div>
   )
