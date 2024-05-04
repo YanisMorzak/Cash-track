1281408ee76c22314f8203993bca65418f81ff38
nom utilisateur de votre repo git <morzak78@gmail.com>
2024-05-04 14:21:20 +0200

    🚧     F05-Dashboard-intro:  Add income and expense Button

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -2,6 +2,7 @@ import prisma from '@/lib/prisma';
 import { currentUser } from '@clerk/nextjs/server';
 import { redirect } from 'next/navigation';
 import React from 'react'
+import { Button } from "@/components/ui/button";
 
 export default async function page() {
   //retrieves the currently logged-in user using the "currentUser()" function
@@ -29,6 +30,21 @@ export default async function page() {
       <div className="border-b bg-card">
         <div className="container flex flex-wrap items-center justify-between gap-6 py-8">
           <p className="text-3xl font-bold">Hello, {user.firstName}! 👋</p>
+          <div className="flex items-center gap-3">
+                <Button
+                  variant={"outline"}
+                  className="border-emerald-500 bg-emerald-950 text-white hover:bg-emerald-700 hover:text-white"
+                >
+                  New income 🤑
+                </Button>
+                      
+                <Button
+                  variant={"outline"}
+                  className="border-rose-500 bg-rose-950 text-white hover:bg-rose-700 hover:text-white"
+                >
+                  New expense 😤
+                </Button>   
+          </div>
         </div>
       </div>
     </div>
