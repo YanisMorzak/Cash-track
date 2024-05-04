068ee3fef35cbb94d84620112be71002bb2048f7
nom utilisateur de votre repo git <morzak78@gmail.com>
2024-05-04 14:08:13 +0200

    🚧     F05-Dashboard-intro:  display user's first nameconfiguration page

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -23,8 +23,14 @@ export default async function page() {
   if (!userSettings) {
     redirect("/wizard");
   }
-  
+
   return (
-    <div>page</div>
+    <div className="h-full bg-background">
+      <div className="border-b bg-card">
+        <div className="container flex flex-wrap items-center justify-between gap-6 py-8">
+          <p className="text-3xl font-bold">Hello, {user.firstName}! 👋</p>
+        </div>
+      </div>
+    </div>
   )
 }
