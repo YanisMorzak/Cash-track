0950fbbcb4f291c8c5d5508c74b755352e6d731b
nom utilisateur de votre repo git <morzak78@gmail.com>
2024-05-04 15:56:54 +0200

    🚧     F05-Dashboard-intro:  add dialog header

--- app/(dashboard)/_components/CreateTransactionDialog.tsx
+++ app/(dashboard)/_components/CreateTransactionDialog.tsx
@@ -1,7 +1,8 @@
 "use client";
 
-import { Dialog, DialogTrigger } from "@/components/ui/dialog";
+import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
 import { TransactionType } from "@/lib/types";
+import { cn } from "@/lib/utils";
 import { ReactNode } from "react";
 
 interface Props {
@@ -14,6 +15,22 @@ interface Props {
     return (
       <Dialog>
         <DialogTrigger asChild>{trigger}</DialogTrigger>
+        <DialogContent>
+        <DialogHeader>
+          <DialogTitle>
+            Create a new{" "}
+            <span
+              className={cn(
+                "m-1",
+                type === "income" ? "text-emerald-500" : "text-red-500"
+              )}
+            >
+              {type}
+            </span>
+            transaction
+          </DialogTitle>
+        </DialogHeader>
+        </DialogContent>
       </Dialog>
     );
   }
