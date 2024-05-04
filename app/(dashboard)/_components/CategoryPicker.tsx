70e5c276c0984b97d290fbbbac0c498d3ea0f9a4
nom utilisateur de votre repo git <morzak78@gmail.com>
2024-05-04 20:12:30 +0200

    🚧     F05-Dashboard-intro:  categoriesQuery which is a useQuery that retrieves categories according to the specified transaction type and automatically manages the caching and loading status of category data.

--- app/(dashboard)/_components/CategoryPicker.tsx
+++ app/(dashboard)/_components/CategoryPicker.tsx
@@ -1,6 +1,8 @@
 "use client"
 
 import { TransactionType } from "@/lib/types";
+import { useQuery } from "@tanstack/react-query";
+import React from "react";
 
 interface Props {
     type: TransactionType;
@@ -8,6 +10,15 @@ interface Props {
   }
   
   function CategoryPicker({ type}: Props) {
+    const [open, setOpen] = React.useState(false);
+    const [value, setValue] = React.useState("");
+  
+    const categoriesQuery = useQuery({
+      queryKey: ["categories", type],
+      queryFn: () =>
+        fetch(`/api/categories?type=${type}`).then((res) => res.json()),
+    });
+  
     return (
         <div>categorypicker</div>
     )
