67249062e1f39064050b9dbdf0ff04b54e07a142
nom utilisateur de votre repo git <morzak78@gmail.com>
2024-05-04 20:54:37 +0200

    🚧     F05-Dashboard-intro:  add a category selection component that uses a popover to display selection options

--- app/(dashboard)/_components/CategoryPicker.tsx
+++ app/(dashboard)/_components/CategoryPicker.tsx
@@ -1,9 +1,13 @@
 "use client"
 
+import { Button } from "@/components/ui/button";
+import { Popover, PopoverTrigger } from "@/components/ui/popover";
 import { TransactionType } from "@/lib/types";
 import { Category } from "@prisma/client";
 import { useQuery } from "@tanstack/react-query";
+import { ChevronsUpDown } from "lucide-react";
 import React from "react";
+import { CategoryRow } from "./CategoryRow";
 
 interface Props {
     type: TransactionType;
@@ -25,7 +29,23 @@ interface Props {
       );
   
     return (
-        <div>categorypicker</div>
+        <Popover open={open} onOpenChange={setOpen}>
+      <PopoverTrigger asChild>
+        <Button
+          variant={"outline"}
+          role="combobox"
+          aria-expanded={open}
+          className="w-[200px] justify-between"
+        >
+          {selectedCategory ? (
+            <CategoryRow category={selectedCategory} />
+          ) : (
+            "Select category"
+          )}
+          <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+        </Button>
+      </PopoverTrigger>
+      </Popover>
     )
   }  
 
