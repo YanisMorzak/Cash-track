a3c06b598f98b1eb6931f2616806144a1d3f2f56
nom utilisateur de votre repo git <morzak78@gmail.com>
2024-05-04 16:21:43 +0200

    🚧     F05-Dashboard-intro:  create form and configured him to use a specific validation schema (CreateTransactionSchema)

--- app/(dashboard)/_components/CreateTransactionDialog.tsx
+++ app/(dashboard)/_components/CreateTransactionDialog.tsx
@@ -3,7 +3,10 @@
 import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
 import { TransactionType } from "@/lib/types";
 import { cn } from "@/lib/utils";
+import { CreateTransactionSchema, CreateTransactionSchemaType } from "@/schema/transaction";
 import { ReactNode } from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 interface Props {
     trigger: ReactNode;
@@ -11,7 +14,13 @@ interface Props {
   }
 
   function CreateTransactionDialog({ trigger, type }: Props) {
-  
+    const form = useForm<CreateTransactionSchemaType>({
+        resolver: zodResolver(CreateTransactionSchema),
+        defaultValues: {
+          type,
+          date: new Date(),
+        },
+      });
     return (
       <Dialog>
         <DialogTrigger asChild>{trigger}</DialogTrigger>
