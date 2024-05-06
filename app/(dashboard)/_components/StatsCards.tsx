017bf73fb95a8fe98339b24ae7bd8573b5d0356e
nom utilisateur de votre repo git <morzak78@gmail.com>
2024-05-06 21:12:31 +0200

    🚧     F07-Stats-Section: display income, expense and balance cards

--- app/(dashboard)/_components/StatsCards.tsx
+++ app/(dashboard)/_components/StatsCards.tsx
@@ -1,10 +1,13 @@
 "use client";
 
 import { GetBalanceStatsResponseType } from "@/app/api/stats/balance/route";
+import SkeletonWrapper from "@/components/SkeletonWrapper";
 import { DateToUTCDate, GetFormatterForCurrency } from "@/lib/helpers";
 import { UserSettings } from "@prisma/client";
 import { useQuery } from "@tanstack/react-query";
+import { TrendingDown, TrendingUp, Wallet } from "lucide-react";
 import { useMemo } from "react";
+import Cards from "./Cards";
 
 interface Props {
     from: Date;
@@ -32,7 +35,39 @@ function StatsCards({ from, to, userSettings }: Props) {
   
     return (
       <div className="relative flex w-full flex-wrap gap-2 md:flex-nowrap">
-       stats
+        <SkeletonWrapper isLoading={statsQuery.isFetching}>
+        <Cards
+          formatter={formatter}
+          value={income}
+          title="Income"
+          icon={
+            <TrendingUp className="h-12 w-12 items-center rounded-lg p-2 text-emerald-500 bg-emerald-400/10" />
+          }
+        />
+      </SkeletonWrapper>
+
+      <SkeletonWrapper isLoading={statsQuery.isFetching}>
+        <Cards
+          formatter={formatter}
+          value={expense}
+          title="Expense"
+          icon={
+            <TrendingDown className="h-12 w-12 items-center rounded-lg p-2 text-red-500 bg-red-400/10" />
+          }
+        />
+      </SkeletonWrapper>
+
+      <SkeletonWrapper isLoading={statsQuery.isFetching}>
+        <Cards
+          formatter={formatter}
+          value={balance}
+          title="Balance"
+          icon={
+            <Wallet className="h-12 w-12 items-center rounded-lg p-2 text-violet-500 bg-violet-400/10" />
+          }
+        />
+      </SkeletonWrapper>
+
       </div>
     );
   }
