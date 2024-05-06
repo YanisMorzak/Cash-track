a5e589f45a2a269fbf8bd88eebb598b472c549dd
nom utilisateur de votre repo git <morzak78@gmail.com>
2024-05-06 23:21:57 +0200

    🚧     F08-Categories-section: call the API (stats -> categories) with react query

--- app/(dashboard)/_components/CategoriesStats.tsx
+++ app/(dashboard)/_components/CategoriesStats.tsx
@@ -1,6 +1,9 @@
 "use client"
 
+import { GetCategoriesStatsResponseType } from '@/app/api/stats/categories/route';
+import { DateToUTCDate } from '@/lib/helpers';
 import { UserSettings } from '@prisma/client';
+import { useQuery } from '@tanstack/react-query';
 import React from 'react'
 
 interface Props {
@@ -10,6 +13,18 @@ interface Props {
   }
 
 export default function CategoriesStats({ userSettings, from, to }: Props) {
+
+    // call the API (stats -> categories) with react query
+    const statsQuery = useQuery<GetCategoriesStatsResponseType>({
+        queryKey: ["overview", "stats", "categories", from, to],
+        queryFn: () =>
+          fetch(
+            `/api/stats/categories?from=${DateToUTCDate(from)}&to=${DateToUTCDate(
+              to
+            )}`
+          ).then((res) => res.json()),
+      });
+    
   return (
     <div>CategoriesStats</div>
   )
