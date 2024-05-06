f5b0538c80e4c53d15ac6bdb1510ca5f74a44f54
nom utilisateur de votre repo git <morzak78@gmail.com>
2024-05-06 20:29:05 +0200

    🚧     F07-Stats-Section: use useMemo, which ensures that this function is only recalculated if the value of userSettings.currency changes, optimizing performance by avoiding unnecessary recalculations. Retrieve statistics data (statsQuery.data) from the statsQuery.

--- app/(dashboard)/_components/StatsCards.tsx
+++ app/(dashboard)/_components/StatsCards.tsx
@@ -1,9 +1,10 @@
 "use client";
 
 import { GetBalanceStatsResponseType } from "@/app/api/stats/balance/route";
-import { DateToUTCDate } from "@/lib/helpers";
+import { DateToUTCDate, GetFormatterForCurrency } from "@/lib/helpers";
 import { UserSettings } from "@prisma/client";
 import { useQuery } from "@tanstack/react-query";
+import { useMemo } from "react";
 
 interface Props {
     from: Date;
@@ -19,6 +20,15 @@ function StatsCards({ from, to, userSettings }: Props) {
           `/api/stats/balance?from=${DateToUTCDate(from)}&to=${DateToUTCDate(to)}`
         ).then((res) => res.json()),
     });
+
+    const formatter = useMemo(() => {
+        return GetFormatterForCurrency(userSettings.currency);
+      }, [userSettings.currency]);
+    
+      const income = statsQuery.data?.income || 0;
+      const expense = statsQuery.data?.expense || 0;
+    
+      const balance = income - expense;
   
     return (
       <div className="relative flex w-full flex-wrap gap-2 md:flex-nowrap">
