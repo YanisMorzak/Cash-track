967b3f40da19bbafd1c28341705d9894681a4102
nom utilisateur de votre repo git <morzak78@gmail.com>
2024-05-06 16:22:13 +0200

    ✨    F06-Dashboard-Overview:  display a title with a search bar to select a date range using the DateRangePicker component.

--- app/(dashboard)/_components/Overview.tsx
+++ app/(dashboard)/_components/Overview.tsx
@@ -1,10 +1,23 @@
 "use client";
 
+import { DateRangePicker } from '@/components/ui/date-range-picker';
 import { UserSettings } from '@prisma/client';
-import React from 'react'
+import { startOfMonth } from 'date-fns';
+import React, { useState } from 'react'
 
 export default function Overview({ userSettings }: { userSettings: UserSettings }) {
+    const [dateRange, setDateRange] = useState<{ from: Date; to: Date }>({
+        from: startOfMonth(new Date()),
+        to: new Date(),
+      });
   return (
-    <div>Overview</div>
+    <>
+      <div className="container flex flex-wrap items-end justify-between gap-2 py-6">
+        <h2 className="text-3xl font-bold">Overview</h2>
+        <div className="flex items-center gap-3">
+          <DateRangePicker/>
+        </div>
+      </div>
+      </>
   )
 }
