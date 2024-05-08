ef6afef2f3d87800ea449581fc77b9a278149e66
nom utilisateur de votre repo git <morzak78@gmail.com>
2024-05-08 17:23:47 +0200

    🚧     F10-Manage-page: add function responsible for deleting a specific category belonging to the currently logged-in user.

--- app/(dashboard)/_actions/categories.ts
+++ app/(dashboard)/_actions/categories.ts
@@ -4,6 +4,8 @@ import prisma from "@/lib/prisma";
 import {
   CreateCategorySchema,
   CreateCategorySchemaType,
+  DeleteCategorySchema,
+  DeleteCategorySchemaType,
 } from "@/schema/categories";
 import { currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
@@ -26,6 +28,30 @@ export async function CreateCategory(form: CreateCategorySchemaType) {
       name,
       icon,
       type,
+    },
+  });
+}
+
+export async function DeleteCategory(form: DeleteCategorySchemaType) {
+  // valide le schéma de la requête de suppression à l'aide du schéma DeleteCategorySchema
+  const parsedBody = DeleteCategorySchema.safeParse(form);
+  if (!parsedBody.success) {
+    throw new Error("bad request");
+  }
+
+  const user = await currentUser();
+  if (!user) {
+    redirect("/sign-in");
+  }
+
+  // utilise les données de la requête pour supprimer une catégorie de la base de données à l'aide de Prisma
+  return await prisma.category.delete({
+    where: {
+      name_userId_type: {
+        userId: user.id,
+        name: parsedBody.data.name,
+        type: parsedBody.data.type,
+      },
     },
   });
 }
