49629af2029cc3d494b6920af948be6d27d28c4d
nom utilisateur de votre repo git <morzak78@gmail.com>
2024-05-08 17:48:23 +0200

    🚧     F10-Manage-page: add category deletion confirmation dialog

--- app/(dashboard)/_components/DeleteCategoryDialog.tsx
+++ app/(dashboard)/_components/DeleteCategoryDialog.tsx
@@ -5,6 +5,8 @@ import { useMutation, useQueryClient } from '@tanstack/react-query';
 import React, { ReactNode } from 'react'
 import { DeleteCategory } from '../_actions/categories';
 import { toast } from 'sonner';
+import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger } from '@/components/ui/alert-dialog';
+import { TransactionType } from '@/lib/types';
 
 interface Props {
     trigger: ReactNode;
@@ -33,6 +35,33 @@ export default function DeleteCategoryDialog({ category, trigger }: Props) {
         },
       });
   return (
-    <div>DeleteCategoryDialog</div>
+    <AlertDialog>
+      <AlertDialogTrigger asChild>{trigger}</AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+          <AlertDialogDescription>
+            This action cannot be undone. This will permanently delete your
+            category
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction
+            onClick={() => {
+              toast.loading("Deleting category...", {
+                id: categoryIdentifier,
+              });
+              deleteMutation.mutate({
+                name: category.name,
+                type: category.type as TransactionType,
+              });
+            }}
+          >
+            Continue
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
   )
 }
