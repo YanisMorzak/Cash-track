e0d48ab6327ce5ae7f03b82e3e8e1c0edf54c681
nom utilisateur de votre repo git <morzak78@gmail.com>
2024-05-08 16:04:09 +0200

    🚧     F10-Manage-page: add header

--- app/(dashboard)/manage/page.tsx
+++ app/(dashboard)/manage/page.tsx
@@ -3,6 +3,18 @@ import React from 'react'
 
 export default function page() {
   return (
-    <div>page</div>
+    <>
+    {/* HEADER */}
+    <div className="border-b bg-card">
+      <div className="container flex flex-wrap items-center justify-between gap-6 py-8">
+        <div>
+          <p className="text-3xl font-bold">Manage</p>
+          <p className="text-muted-foreground">
+            Manage your account settings and categories
+          </p>
+        </div>
+      </div>
+    </div>
+    </>
   )
 }
