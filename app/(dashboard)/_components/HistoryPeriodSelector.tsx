0b081cd7a03ab5cd900aa736debe8abb9066aec9
nom utilisateur de votre repo git <morzak78@gmail.com>
2024-05-08 10:00:16 +0200

    🚧       F09-History-section: call the history periods api

--- app/(dashboard)/_components/HistoryPeriodSelector.tsx
+++ app/(dashboard)/_components/HistoryPeriodSelector.tsx
@@ -1,6 +1,10 @@
 "use client";
 
+import { GetHistoryPeriodsResponseType } from '@/app/api/history-periods/route';
+import SkeletonWrapper from '@/components/SkeletonWrapper';
+import { Tabs, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import { Period, Timeframe } from '@/lib/types';
+import { useQuery } from '@tanstack/react-query';
 import React from 'react'
 
 interface Props {
@@ -17,7 +21,23 @@ export default function HistoryPeriodSelector({
     timeframe,
     setTimeframe,
   }: Props) {
+    const historyPeriods = useQuery<GetHistoryPeriodsResponseType>({
+        queryKey: ["overview", "history", "periods"],
+        queryFn: () => fetch(`/api/history-periods`).then((res) => res.json()),
+      });
   return (
-    <div>HistoryPeriodSelector</div>
+    <div className="flex flex-wrap items-center gap-4">
+    <SkeletonWrapper isLoading={historyPeriods.isFetching} fullWidth={false}>
+      <Tabs
+        value={timeframe}
+        onValueChange={(value) => setTimeframe(value as Timeframe)}
+      >
+        <TabsList>
+          <TabsTrigger value="year">Year</TabsTrigger>
+          <TabsTrigger value="month">Month</TabsTrigger>
+        </TabsList>
+      </Tabs>
+    </SkeletonWrapper>
+    </div>
   )
 }
