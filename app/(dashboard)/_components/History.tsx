b12c157c6a494e29ba72e572cb2088e0a1132594
nom utilisateur de votre repo git <morzak78@gmail.com>
2024-05-08 09:38:39 +0200

    🚧       F09-History-section: define timeframe and period state

--- app/(dashboard)/_components/History.tsx
+++ app/(dashboard)/_components/History.tsx
@@ -1,9 +1,16 @@
 "use client"
 
+import { Period, Timeframe } from '@/lib/types';
 import { UserSettings } from '@prisma/client'
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function History({ userSettings }: { userSettings: UserSettings }) {
+    const [timeframe, setTimeframe] = useState<Timeframe>("month");
+    const [period, setPeriod] = useState<Period>({
+      month: new Date().getMonth(),
+      year: new Date().getFullYear(),
+    });
+  
   return (
     <div>History</div>
   )
