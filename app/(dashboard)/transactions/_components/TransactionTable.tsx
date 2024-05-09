a832123a1a1da3ad1a3f46db14444bc43de1a59f
nom utilisateur de votre repo git <morzak78@gmail.com>
2024-05-09 12:00:51 +0200

    🚧    F11-Transaction-Page: call the transaction api

--- app/(dashboard)/transactions/_components/TransactionTable.tsx
+++ app/(dashboard)/transactions/_components/TransactionTable.tsx
@@ -1,5 +1,8 @@
 "use client"
 
+import { GetTransactionHistoryResponseType } from '@/app/api/transactions-history/route';
+import { DateToUTCDate } from '@/lib/helpers';
+import { useQuery } from '@tanstack/react-query';
 import React from 'react'
 
 interface Props {
@@ -8,6 +11,15 @@ interface Props {
   }
 
 export default function TransactionTable({ from, to }: Props) {
+    const history = useQuery<GetTransactionHistoryResponseType>({
+        queryKey: ["transactions", "history", from, to],
+        queryFn: () =>
+          fetch(
+            `/api/transactions-history?from=${DateToUTCDate(
+              from
+            )}&to=${DateToUTCDate(to)}`
+          ).then((res) => res.json()),
+      });
   return (
     <div>TransactionTable</div>
   )
