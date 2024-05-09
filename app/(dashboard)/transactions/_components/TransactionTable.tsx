69e5d35976eb0ba6d1442ba5c4141119c547df10
nom utilisateur de votre repo git <morzak78@gmail.com>
2024-05-09 12:25:24 +0200

    🚧    F11-Transaction-Page: add category column to TransactionTable

--- app/(dashboard)/transactions/_components/TransactionTable.tsx
+++ app/(dashboard)/transactions/_components/TransactionTable.tsx
@@ -3,12 +3,42 @@
 import { GetTransactionHistoryResponseType } from '@/app/api/transactions-history/route';
 import { DateToUTCDate } from '@/lib/helpers';
 import { useQuery } from '@tanstack/react-query';
+import {
+    ColumnDef,
+    flexRender,
+    getCoreRowModel,
+    useReactTable,
+  } from "@tanstack/react-table"
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+  } from "@/components/ui/table"
 import React from 'react'
 
 interface Props {
     from: Date;
     to: Date;
   }
+
+  const emptyData: any[] = [];
+
+  type TransactionHistoryRow = GetTransactionHistoryResponseType[0];
+
+const columns: ColumnDef<TransactionHistoryRow>[] = [
+    {
+        accessorKey: "category",
+        cell: ({ row }) => (
+          <div className="flex gap-2 capitalize">
+            {row.original.categoryIcon}
+            <div className="capitalize">{row.original.category}</div>
+          </div>
+        ),
+      },
+]
 
 export default function TransactionTable({ from, to }: Props) {
     const history = useQuery<GetTransactionHistoryResponseType>({
@@ -20,7 +50,56 @@ export default function TransactionTable({ from, to }: Props) {
             )}&to=${DateToUTCDate(to)}`
           ).then((res) => res.json()),
       });
+
+      const table = useReactTable({
+        data: history.data || emptyData,
+        columns,
+        getCoreRowModel: getCoreRowModel(),
+      });
   return (
-    <div>TransactionTable</div>
+    <div className="rounded-md border">
+    <Table>
+      <TableHeader>
+        {table.getHeaderGroups().map((headerGroup) => (
+          <TableRow key={headerGroup.id}>
+            {headerGroup.headers.map((header) => {
+              return (
+                <TableHead key={header.id}>
+                  {header.isPlaceholder
+                    ? null
+                    : flexRender(
+                        header.column.columnDef.header,
+                        header.getContext()
+                      )}
+                </TableHead>
+              )
+            })}
+          </TableRow>
+        ))}
+      </TableHeader>
+      <TableBody>
+        {table.getRowModel().rows?.length ? (
+          table.getRowModel().rows.map((row) => (
+            <TableRow
+              key={row.id}
+              data-state={row.getIsSelected() && "selected"}
+            >
+              {row.getVisibleCells().map((cell) => (
+                <TableCell key={cell.id}>
+                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                </TableCell>
+              ))}
+            </TableRow>
+          ))
+        ) : (
+          <TableRow>
+            <TableCell colSpan={columns.length} className="h-24 text-center">
+              No results.
+            </TableCell>
+          </TableRow>
+        )}
+      </TableBody>
+    </Table>
+  </div>
   )
 }
