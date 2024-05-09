768cdf999a990d792777e1f74a46ee314123c12d
nom utilisateur de votre repo git <morzak78@gmail.com>
2024-05-09 11:32:37 +0200

    🚧     F11-Transaction-Page: add page to transactions folder

---
+++ app/(dashboard)/transactions/page.tsx
@@ -0,0 +1,17 @@
+"use client"
+
+import React from 'react'
+
+export default function page() {
+  return (
+    <>
+    <div className="border-b bg-card">
+      <div className="container flex flex-wrap items-center justify-between gap-6 py-8">
+        <div>
+          <p className="text-3xl font-bold">Transactions history</p>
+        </div>
+      </div>
+    </div>
+    </>
+  )
+}
