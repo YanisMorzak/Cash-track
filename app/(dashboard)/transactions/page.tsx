928c2658ebc39df5ef99a5fdcfa5b3158e0606d4
nom utilisateur de votre repo git <morzak78@gmail.com>
2024-05-09 11:38:24 +0200

    ✨   F11-Transaction-Page: add DateRangePicker

--- app/(dashboard)/transactions/page.tsx
+++ app/(dashboard)/transactions/page.tsx
@@ -1,8 +1,16 @@
 "use client"
 
-import React from 'react'
+import { DateRangePicker } from '@/components/ui/date-range-picker';
+import { MAX_DATE_RANGE_DAYS } from '@/lib/constants';
+import { differenceInDays, startOfMonth } from 'date-fns';
+import React, { useState } from 'react'
+import { toast } from 'sonner';
 
 export default function page() {
+    const [dateRange, setDateRange] = useState<{ from: Date; to: Date }>({
+        from: startOfMonth(new Date()),
+        to: new Date(),
+      });
   return (
     <>
     <div className="border-b bg-card">
@@ -10,6 +18,25 @@ export default function page() {
         <div>
           <p className="text-3xl font-bold">Transactions history</p>
         </div>
+        <DateRangePicker
+            initialDateFrom={dateRange.from}
+            initialDateTo={dateRange.to}
+            showCompare={false}
+            onUpdate={(values) => {
+              const { from, to } = values.range;
+              // We update the date range only if both dates are set
+
+              if (!from || !to) return;
+              if (differenceInDays(to, from) > MAX_DATE_RANGE_DAYS) {
+                toast.error(
+                  `The selected date range is too big. Max allowed range is ${MAX_DATE_RANGE_DAYS} days!`
+                );
+                return;
+              }
+
+              setDateRange({ from, to });
+            }}
+          />
       </div>
     </div>
     </>
